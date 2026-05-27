8888f90e87d562791efb74451bb7a988e04ccca2
Chin Hongnyheng <chinhongnyheng@Chins-MacBook-Pro.local>
2026-05-27 20:59:47 +0700

    Exam #3

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,12 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('UserService', () => {
   let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn().mockReturnValue('mock-token'),
+            verify: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
